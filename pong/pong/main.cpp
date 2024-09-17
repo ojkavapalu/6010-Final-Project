39435827f75948aa3dd5df9b0fb0aab58be56080
Jose <u1541723@umail.utah.edu>
2024-09-17 14:18:08 -0600

    push final project

---
+++ pong/pong/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  pong
+//
+//  Created by Jose Bonilla on 9/17/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
