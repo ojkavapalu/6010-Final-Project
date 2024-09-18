17b09e8618ddd910323dfca11bc4e9a88bfacf36
Jose <u1541723@umail.utah.edu>
2024-09-18 16:07:05 -0600

    commit pong project

--- pong/main.cpp
+++ pong/main.cpp
@@ -11,106 +11,11 @@
 #include <iostream>
 #include <memory>
 
-/*
-class paddle{
-    
-    float paddleWidth = 25.f;
-    float paddleHeight = 100.f;
-    const float paddleSpeed = 400.f;
-    
-    
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-};
-
-
-
-class ball{
-    const float ballRadius = 10.f;
-    const float ballSpeed = 300.f;
-    
-    
-};
-
-
-
-
-class game{
-
-public:
-    
-    //creates window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Pong");
-    
-    //sets widown frames
-    window.setFramerateLimit(60);
-    
-    sf::RectangleShape leftPaddle(sf::Vector2f(paddleWidth, paddleHeight));
-    sf::RectangleShape rightPaddle(sf::Vector2f(paddleWidth, paddleHeight));
-    sf::CircleShape ball(ballRadius);
-    
-    
-    
-    
-    
-    
-    
-}
-
-
-int main() {
-
-
-
-    // Set paddles and ball initial positions
-    leftPaddle.setPosition(50.f, 300.f - paddleHeight / 2);
-    rightPaddle.setPosition(750.f - paddleWidth, 300.f - paddleHeight / 2);
-    ball.setPosition(400.f, 300.f);
-
-    // Initialize velocity for ball movement
-    sf::Vector2f ballVelocity(-ballSpeed, -ballSpeed);
-
-    // Main game loop
-    while (window.isOpen()) {
-        // Process events
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-        }
-
-        // Clear the screen
-        window.clear(sf::Color::Black);
-
-        // Draw paddles and ball
-        window.draw(leftPaddle);
-        window.draw(rightPaddle);
-        window.draw(ball);
-
-        // Display everything
-        window.display();
-    }
-
-    return 0;
-}
-
-*/
-
-
 const float paddleWidth = 25.f;
 const float paddleHeight = 100.f;
 const float ballRadius = 10.f;
 const float paddleSpeed = 400.f;
-const float ballSpeed = 300.f;
+//const float ballSpeed = 300.f;
 
 int main() {
     // Create a game window
@@ -123,17 +28,23 @@ int main() {
     sf::RectangleShape leftPaddle(sf::Vector2f(paddleWidth, paddleHeight));
     sf::RectangleShape rightPaddle(sf::Vector2f(paddleWidth, paddleHeight));
     sf::CircleShape ball(ballRadius);
+    
+    
 
     // Set paddles and ball initial positions
     leftPaddle.setPosition(50.f, 300.f - paddleHeight / 2);
     rightPaddle.setPosition(750.f - paddleWidth, 300.f - paddleHeight / 2);
-    ball.setPosition(780.f, 1.f);
+    ball.setPosition(400.f, 300.f);
+    
+    float xstep = 4;
+    float ystep = 4;
 
-    // Initialize velocity for ball movement
-    sf::Vector2f ballVelocity(-ballSpeed, -ballSpeed);
     
     
-    
+    sf::Sprite player;
+    player.setRotation(90);
+    //player.setTexture(500, 200);
+    player.setPosition(400.0f, 500.0f);  // Start player at a position
 
     // Main game loop
     while (window.isOpen()) {
@@ -143,8 +54,31 @@ int main() {
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
+            
         }
+        
 
+        ball.move(xstep, ystep);
+        if (ball.getPosition().x > 800-20){
+            
+            xstep = -xstep; //change the direction
+            
+        }
+        else if (ball.getPosition().x < 0 + 20){
+            
+            xstep = -xstep;
+        }
+        
+        ball.move(xstep, ystep);
+        if (ball.getPosition().y > 600-20){
+            
+            ystep = -ystep; //change the direction
+            
+        }
+        else if (ball.getPosition().y < 0 + 20){
+            
+            ystep = -ystep;
+        }
         // Clear the screen
         window.clear(sf::Color::Black);
 
@@ -159,4 +93,7 @@ int main() {
 
     return 0;
 }
+
+
+
 
